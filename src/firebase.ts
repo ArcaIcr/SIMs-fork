4d75a065e484817a647f73c859141b38e2b7518a
carmina000 <carminayanez7@gmail.com>
2025-05-06 16:37:07 +0800

    chore: ENV

--- src/firebase.ts
+++ src/firebase.ts
@@ -8,13 +8,13 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDDRQT-8VxArJF3aoYwBJdBFNXoFI4b9L4",
-  authDomain: "sims-2c563.firebaseapp.com",
-  projectId: "sims-2c563",
-  storageBucket: "sims-2c563.firebasestorage.app",
-  messagingSenderId: "38506610828",
-  appId: "1:38506610828:web:f15e367f5d416cafb97ba9",
-  measurementId: "G-KVXK1WJDN6"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
